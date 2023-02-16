a3ae4907ff67bb71a30f8192b7072dd8ccb5a4b4
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-16 15:42:38 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import PokemonForm from './PokemonForm';
+import PokemonInfo from './PokemonInfo';
+
+export default class App extends Component {
+  state = {
+    pokemonName: '',
+  };
+
+  handleFormSubmit = pokemonName => {
+    this.setState({ pokemonName });
+  };
+
+  render() {
+    return (
+      <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
+        <PokemonForm onSubmit={this.handleFormSubmit} />
+        <PokemonInfo pokemonName={this.state.pokemonName} />
+        <ToastContainer autoClose={3000} />
+      </div>
+    );
+  }
+}
